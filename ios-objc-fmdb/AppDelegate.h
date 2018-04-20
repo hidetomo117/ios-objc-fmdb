01616f2925057a39368f2c2c2b3397cfd71ccf19
OkuderaYuki <>
2018-04-21 02:46:25 +0900

    first commit

---
+++ ios-objc-fmdb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios-objc-fmdb
+//
+//  Created by OkuderaYuki on 2018/04/21.
+//  Copyright © 2018年 OkuderaYuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios-objc-fmdb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios-objc-fmdb
+//
+//  Created by OkuderaYuki on 2018/04/21.
+//  Copyright © 2018年 OkuderaYuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
